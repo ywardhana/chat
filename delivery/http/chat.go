4fc6cd7f3d473e63ca478c2fecab5b173293a681
ywardhana <muhammad.yasher@gmail.com>
2019-12-01 17:43:14 +0700

    init chat handler init

--- delivery/http/chat.go
+++ delivery/http/chat.go
@@ -1,1 +1,21 @@
 package http
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/ywardhana/chat/app/system/middleware"
+)
+
+type ChatHandler struct {
+	// usecase chat.
+}
+
+func (h *ChatHandler) Register(router *httprouter.Router, m *middleware.Middleware) {
+	router.GET("/coba", m.AuthBasic(h.TestFunc))
+	// router.POST("/chat", m.AuthBasic(h.CreateChatHandler))
+}
+
+func (h *ChatHandler) TestFunc(w http.ResponseWriter, r *http.Request, param httprouter.Params) error {
+	return nil
+}
