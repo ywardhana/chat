d4818776ed0bd76abc5b24346f2b79a244250fa5
ywardhana <muhammad.yasher@gmail.com>
2019-12-01 19:35:41 +0700

    delete test handler

--- delivery/http/chat.go
+++ delivery/http/chat.go
@@ -1,8 +1,6 @@
 package http
 
 import (
-	"net/http"
-
 	"github.com/julienschmidt/httprouter"
 	"github.com/ywardhana/chat/app/system/middleware"
 	"github.com/ywardhana/chat/chat"
@@ -19,10 +17,5 @@ func NewChatHandler(chatUC chat.ChatUsecase) *ChatHandler {
 }
 
 func (h *ChatHandler) Register(router *httprouter.Router, m *middleware.Middleware) {
-	router.GET("/coba", m.AuthBasic(h.TestFunc))
 	router.POST("/chat", m.AuthBasic(h.CreateChat))
-}
-
-func (h *ChatHandler) TestFunc(w http.ResponseWriter, r *http.Request, param httprouter.Params) error {
-	return nil
 }
