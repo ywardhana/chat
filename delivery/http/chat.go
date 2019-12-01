625f695c6ddcde561d446f511821e4d484c17383
ywardhana <muhammad.yasher@gmail.com>
2019-12-01 18:51:29 +0700

    create chat handler

--- delivery/http/chat.go
+++ delivery/http/chat.go
@@ -5,15 +5,22 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/ywardhana/chat/app/system/middleware"
+	"github.com/ywardhana/chat/chat"
 )
 
 type ChatHandler struct {
-	// usecase chat.
+	usecase chat.ChatUsecase
+}
+
+func NewChatHandler(chatUC chat.ChatUsecase) *ChatHandler {
+	return &ChatHandler{
+		usecase: chatUC,
+	}
 }
 
 func (h *ChatHandler) Register(router *httprouter.Router, m *middleware.Middleware) {
 	router.GET("/coba", m.AuthBasic(h.TestFunc))
-	// router.POST("/chat", m.AuthBasic(h.CreateChatHandler))
+	router.POST("/chat", m.AuthBasic(h.CreateChat))
 }
 
 func (h *ChatHandler) TestFunc(w http.ResponseWriter, r *http.Request, param httprouter.Params) error {
